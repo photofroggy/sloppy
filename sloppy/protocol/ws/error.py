b4a83d9aa5cafb5c9bd24a8440dab81c0a5866c4
Henry Rapley <froggywillneverdie@msn.com>
2012-07-28 13:22:26 +0100

    Added error thing. More progress on the server side protocol

---
+++ sloppy/protocol/ws/error.py
@@ -0,0 +1,12 @@
+''' sloppy.protocol.ws.error - photofroggy
+    Exceptions for the WebSocket stuff.
+'''
+
+
+class WSHandshakeError(Exception):
+    """
+    An error happened when trying to handshake.
+    """
+
+
+
--- sloppy/protocol/ws/flow.py
+++ sloppy/protocol/ws/flow.py
@@ -2,29 +2,17 @@
     WebSocket flow control.
 '''
 from sloppy.flow import Protocol
+from sloppy.flow import ServerFactory
 from sloppy.flow import ConnectionFactory
 from sloppy.protocol import http
 
 
-class WebSocketServerFactory(ConnectionFactory):
+class WebSocketServerFactory(ServerFactory):
     """
     WebSocket server factory.
     
     This factory serves client connections on a server.
     """
-    
-    def __init___(self, protocol=None, *args, **kwargs):
-        """
-        Store the protocol class to be used for connections.
-        """
-        self._protocol = protocol or WebSocketServerProtocol
-        self.init(*args, **kwargs)
-    
-    def protocol(self):
-        """
-        Return appropriate protocol object.
-        """
-        return self._protocol()
 
 
 class WebSocketServerProtocol(Protocol):
@@ -32,6 +20,43 @@ class WebSocketServerProtocol(Protocol):
     Base protocol object for WebSocket server connections.
     """
     
+    def __init__(self):
+        self.handshaked = False
+        self._buffer = ''
+    
+    def data_received(self, data):
+        """
+        Called when data is received on the connection.
+        """
+        self._buffer = ''.join(self._buffer, data.decode())
+        
+        if not '\0' in self._buffer:
+            return
+        
+        if self._buffer.find('\0') == len(self._buffer) - 1:
+            data = self._buffer
+            self._buffer = ''
+        else:
+            data = self._buffer.split('\0')
+            self._buffer = data[-1]
+            data = data[:-1]
+        
+        for packet in data:
+        
+            if not self.handshaked:
+                request = http.Request(packet)
+                # Has to be a GET
+                if request.command is None or request.command != 'GET':
+                    continue
+                # Needs to be at least HTTP/1.1
+                if float(request.request_version.split('/')[-1]) < 1.1:
+                    continue
+                # Should have headers, and we need an upgrade header.
+                if not hasattr(request, 'headers') or 'upgrade' not in request.headers:
+                    continue
+                # Needs to be requesting a WebSocket connection.
+                if 'websocket' not in request.headers['upgrade'].lower():
+                    continue
     
 
 
