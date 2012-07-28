ac9ba38dc658f7e3834b705bf5c44e39d2c2c123
Henry Rapley <froggywillneverdie@msn.com>
2012-07-28 14:29:18 +0100

    lols

--- sloppy/protocol/ws/flow.py
+++ sloppy/protocol/ws/flow.py
@@ -5,6 +5,7 @@ from sloppy.flow import Protocol
 from sloppy.flow import ServerFactory
 from sloppy.flow import ConnectionFactory
 from sloppy.protocol import http
+from sloppy.protocol.ws.error import WSHandshakeError
 
 
 class WebSocketServerFactory(ServerFactory):
@@ -13,6 +14,12 @@ class WebSocketServerFactory(ServerFactory):
     
     This factory serves client connections on a server.
     """
+    
+    def protocol(self):
+        """
+        Return appropriate protocol object.
+        """
+        return self._protocol(self)
 
 
 class WebSocketServerProtocol(Protocol):
@@ -20,9 +27,18 @@ class WebSocketServerProtocol(Protocol):
     Base protocol object for WebSocket server connections.
     """
     
-    def __init__(self):
+    def __init__(self, factory):
         self.handshaked = False
         self._buffer = ''
+        self._factory = factory
+    
+    def connected(self, transport):
+        """
+        A new client has connected to the server.
+        
+        Store the transport.
+        """
+        self._transport = transport
     
     def data_received(self, data):
         """
@@ -47,16 +63,47 @@ class WebSocketServerProtocol(Protocol):
                 request = http.Request(packet)
                 # Has to be a GET
                 if request.command is None or request.command != 'GET':
-                    continue
+                    self._factory.fail(self._transport,
+                        WSHandshakeError('Client sending data without handshaking'))
+                    return
                 # Needs to be at least HTTP/1.1
                 if float(request.request_version.split('/')[-1]) < 1.1:
-                    continue
+                    self._factory.fail(self._transport,
+                        WSHandshakeError('Incompatible HTTP version'))
+                    return
                 # Should have headers, and we need an upgrade header.
                 if not hasattr(request, 'headers') or 'upgrade' not in request.headers:
-                    continue
+                    self._factory.fail(self._transport,
+                        WSHandshakeError('No header values given'))
+                    return
                 # Needs to be requesting a WebSocket connection.
                 if 'websocket' not in request.headers['upgrade'].lower():
-                    continue
+                    self._factory.fail(self._transport,
+                        WSHandshakeError('Invalid Upgrade header'))
+                    return
+                # Needs to be requesting a connection upgrade.
+                if 'connection' not in request.headers or 'upgrade' not in request.headers['connection'].lower():
+                    self._factory.fail(self._transport,
+                        WSHandshakeError('Invalid Connection header'))
+                    return
+                # Needs to be using WebSocket protocol version 13.
+                if 'sec-websocket-version' not in request.headers or request.headers['sec-websocket-version'] != '13':
+                    self._factory.fail(self._transport,
+                        WSHandshakeError('Incompatible WebSocket version'))
+                    return
+                # Needs to provide a key.
+                if 'sec-websocket-key' not in request.headers:
+                    self._factory.fail(self._transport,
+                        WSHandshakeError('No key provided'))
+                    return
+                
+                # Ok, now we let the handshake method take over.
+                self.handshake(request)
+    
+    def on_handshake(self):
+        """
+        Handshake received.
+        """
     
 
 
